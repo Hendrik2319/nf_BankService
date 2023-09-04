e5fb79842ef802e044e5441ca3cb48d01d139503
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-04 15:02:39 +0200

    added first code files

---
+++ src/main/java/org/example/bankservice/Account.java
@@ -0,0 +1,4 @@
+package org.example.bankservice;
+
+public class Account {
+}
---
+++ src/main/java/org/example/bankservice/Client.java
@@ -0,0 +1,4 @@
+package org.example.bankservice;
+
+public record Client(String firstname, String lastName, int customerNumber) {
+}
