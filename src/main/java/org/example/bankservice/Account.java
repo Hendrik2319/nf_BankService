7ac26849eac2187f137a3e3d7329d1cbc28d3a56
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-04 15:17:40 +0200

    feat: added Account

--- src/main/java/org/example/bankservice/Account.java
+++ src/main/java/org/example/bankservice/Account.java
@@ -1,4 +1,23 @@
 package org.example.bankservice;
 
+import java.math.BigDecimal;
+
 public class Account {
+    private final String accountNumber;
+    private BigDecimal balance;
+    private final Client client;
+
+    public Account(String accountNumber, BigDecimal balance, Client client) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.client = client;
+    }
+
+    public void deposit(double money) {
+        balance = balance.add(BigDecimal.valueOf(money));
+    }
+
+    public void withdraw(double money) {
+        balance = balance.subtract(BigDecimal.valueOf(money));
+    }
 }
--- src/main/java/org/example/bankservice/Main.java
+++ src/main/java/org/example/bankservice/Main.java
@@ -1,6 +1,34 @@
 package org.example.bankservice;
 
 public class Main {
+    /*
+
+        Erstellt eine Record 'Client' mit den Eigenschaften Vorname, Nachname und
+        Kundennummer (wähle geeignete englische Feldnamen).
+
+        Erstellt eine Klasse 'Account' (kein Record, soll vorerst veränderlich sein)
+        mit den Eigenschaften Kontonummer (String), Kontostand (BigDecimal) und dem zugehörigen Kunden.
+
+        Bitte poste hier den Link zu eurem GitHub-Repository, in dem ihr eure Lösungen teilt.
+
+
+
+        Nun implementieren wir convenience-Methoden, um den Kontostand zu ermitteln und zu verändern.
+
+        Implementiert eine Methode, um Geld auf das Konto einzuzahlen.
+        Implementiert eine Methode, um Geld vom Konto abzuheben.
+
+
+
+        Nun implementieren wir eine Klasse, die eine Liste von Kunden und deren Konten verwaltet.
+
+        Erstellt eine Klasse 'BankService', die eine Menge von Konten verwaltet.
+        Implementiert eine Methode im BankService, um ein Konto zu eröffnen. Man soll dafür nur einen
+        Kunden als Argument übergeben müssen. Es soll die neue Kontonummer zurückgeben.
+        Implementiert eine Methode im BankService, von einer Kontonummer (als String) einen bestimmten
+        Betrag (als BigDecimal) auf eine andere Kontonummer (als String) zu überweisen.
+
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
